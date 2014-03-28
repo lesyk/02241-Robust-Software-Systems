da2e133429d045e818eefa2a73e4fc3afbd367c7
Victor Lesyk <lesyk.victor@gmail.com>
2014-03-28 18:21:48 +0100

    tests for duplication added

---
+++ test/CopyOfRabinKarpClass.java
@@ -0,0 +1,16 @@
+package morpheusmatrix;
+
+public class CopyOfRabinKarpClass {
+	public int check() {
+		int a = 22;
+		
+		int b = 22;
+		int c = 22;
+		return c;
+	}
+	
+	public int checks() {
+		int a = 22;
+		return a;
+	}
+}
